38ff8b22f38f06dee2690bc6452b38e75720e55f
000namc <000namc@gmail.com>
2024-11-11 00:09:47 +0000

    실험 초기 세팅

--- vision/src/train.py
+++ vision/src/train.py
@@ -10,14 +10,16 @@ import gc
 import cv2
 import torch
 import torch.nn.functional as F
-from torch import nn
 from torch.utils.data import DataLoader, Dataset
 from torch.optim import AdamW
-import wandb
 import argparse
 import json
+from torch import nn, optim
+from torchvision import datasets, transforms
 from utils.logger import setup_logger
 from utils.experiment_committer import git_push_process
+from models.alexnet import AlexNet
+import wandb 
 
 parser = argparse.ArgumentParser(description="Git push for specific experiment")
 parser.add_argument('--model_name', required=True, help='The name of the model')
@@ -28,6 +30,7 @@ class cfg:
     seed = 123
     model_name = args.model_name
     experiment_name = args.experiment_name
+    num_epochs = 10
 
 # git_push_process(model_name = cfg.model_name, experiment_name = cfg.experiment_name)
 
@@ -54,3 +57,68 @@ def set_seed(seed = 42):
     print('> SEEDING DONE')
     
 set_seed(cfg.seed)
+
+
+transform = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()])
+train_ds = datasets.ImageFolder(root='/app/data/paper_implementations/vision/imagenet/train', transform = transform)
+train_dl = DataLoader(train_ds, batch_size=64, shuffle=True, num_workers=4)
+valid_ds = datasets.ImageFolder(root='/app/data/paper_implementations/vision/imagenet/valid', transform = transform)
+valid_dl = DataLoader(valid_ds, batch_size=64, shuffle=True, num_workers=4)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = AlexNet(num_classes=1000, dropout=0.5)
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+
+for epoch in range(1, cfg.num_epochs+1):
+    model.train()
+    total_loss = 0.0
+
+    # training phase
+    with tqdm(train_dl, leave=True) as pbar:
+        optimizer.zero_grad()
+
+        for idx, images, labels in enumerate(pbar):
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+    train_loss = total_loss/len(train_dl)
+    if cfg.experiment_name != '':
+        wandb.log({"epoch": epoch, "train_loss": train_loss})
+
+    # evaluation phase
+    model.eval()  
+    correct = 0
+    total = 0
+    total_val_loss = 0.0
+
+    with torch.no_grad():  # 평가 시에는 그래디언트 계산을 하지 않음
+        for images, labels in valid_dl:
+            images, labels = images.to(device), labels.to(device)
+
+            # 순전파
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # 정확도 계산
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            total_val_loss += loss.item()
+
+    val_loss = total_val_loss / len(valid_dl)
+    accuracy = 100 * correct / total
+
+    if cfg.experiment_name != '':
+        wandb.log({"epoch": epoch, "val_loss": val_loss, "val_accuracy": accuracy})
